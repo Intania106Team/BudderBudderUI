6652208d833e19aee09d42c5862085939c577f51
Thanapat Chotipun <66824385+PatrickChoDev@users.noreply.github.com>
2023-03-28 05:10:37 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  websocket_url: import.meta.env.WS_URL || "ws://localhost:5501/chats",
-  backend_url: import.meta.env.BACKEND_URL || "http://localhost:5500",
+  websocket_url: import.meta.env.WS_URL || "ws://168.138.196.138:54594/chats",
+  backend_url: import.meta.env.BACKEND_URL || "http://168.138.196.138:54593",
 };
