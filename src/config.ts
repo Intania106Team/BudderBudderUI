72bf10e4c2716dc6a26529aa073ecfbe99bbab1f
Thanapat Chotipun <66824385+PatrickChoDev@users.noreply.github.com>
2023-03-28 07:34:36 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  websocket_url: import.meta.env.WS_URL || "wss://api-buddybudder.intania106.tech:54594/chats",
-  backend_url: import.meta.env.BACKEND_URL || "https://api-buddybudder.intania106.tech:54593",
+  websocket_url: import.meta.env.WS_URL || "wss://api-buddybudder.intania106.tech:8080/chats",
+  backend_url: import.meta.env.BACKEND_URL || "https://api-buddybudder.intania106.tech",
 };
